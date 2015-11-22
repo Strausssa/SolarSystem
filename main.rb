d9c6a1fea4ee70dcc275284e72c4c059116debbe
Sam <strausssa@s.dcsdk12.org>
2015-11-22 13:28:11 -0700

    Better

--- main.rb
+++ main.rb
@@ -1,36 +1,52 @@
 require "gosu"
-require_relative "sun"
-require_relative "planet"
+require_relative "planets"
 
-class SolarSystemWindow < Gosu::Window
+class Solar < Gosu::Window
 
-  G = 6.67408e-11 
-
-  def initialize plan
-    @background_image = Gosu::Image.new("images/space.jpg")
+  G = 6.67408e-11
+  
+  attr_accessor :radius ,:G
+  
+  def initialize
     @window_size = 750
     super @window_size, @window_size
-    @data = File.read("#{plan}")
-    @lines = @data.split("\n")
+    @background_image = Gosu::Image.new("images/space.jpg")
+    data=File.read("planets.txt")
+    lines = data.split("\n")
+    @number_of_planets = lines[0]
+    @bigradius = lines[1].to_f
+    @radius = (@bigradius/750).to_f
+    @planets_data = lines[2...lines.length]
     @planets = []
-    @planets_data = @lines[2..6].map {|planet| planet.split(" ") }
-    @planets_data.each { |planet| @planets.push(Planet.new(planet, @lines[1].to_f*2, @window_size)) }
+    @planets_data.each {|planet| 
+      planet = planet.split(" ")
+      @planets.push(Planets.new(planet[0].to_f,planet[1].to_f,planet[2].to_f,planet[3].to_f,planet[4].to_f,planet[5],radius.to_f))
+    }
   end
 
   def update
+    @planets.each do |planet|
+      for i in 0...@planets.length
+        if @planets[i].name != planet.name
+            planet.total_force(@planets[i])
 
+          end
+       end
+       
+      planet.acceleration
+      planet.velocity
+      planet.position
+       
+    end
   end
-
-  def button_down id
-    close if id == Gosu::KbEscape
-  end
-
+  
   def draw
     @background_image.draw(0,0,0)
-    @planets.each { |planet| planet.draw }
+    @planets.each do |planet|
+      planet.draw
+    end
   end
-
 end
 
-window = SolarSystemWindow.new("simulations/planets.txt")
+window = Solar.new
 window.show
